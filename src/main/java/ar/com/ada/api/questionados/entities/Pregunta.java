f1a53d5ae88cdd0ee64df257f8f9d83af9f91141
lumaldonado <lulimaldonado98@hotmail.com>
2021-07-18 19:18:17 -0300

    Se crean los package de controllers, services, repos, models y entities, y las entities de pregunta, respuesta y categoria

---
+++ src/main/java/ar/com/ada/api/questionados/entities/Pregunta.java
@@ -0,0 +1,67 @@
+package ar.com.ada.api.questionados.entities;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "pregunta")
+public class Pregunta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pregunta_id")
+    private Integer preguntaId;
+
+    @Column(name = "enunciado")
+    private String enunciado;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", referencedColumnName = "categoria_id")
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "pregunta", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<Respuesta> opciones = new ArrayList<>();
+
+    public void addRespuesta(Respuesta respuesta) {
+        this.opciones.add(respuesta);
+    }
+
+    public Integer getPreguntaId() {
+        return preguntaId;
+    }
+
+    public void setPreguntaId(Integer preguntaId) {
+        this.preguntaId = preguntaId;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+        this.categoria.addPregunta(this);
+
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public List<Respuesta> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(List<Respuesta> opciones) {
+        this.opciones = opciones;
+    }
+
+}
